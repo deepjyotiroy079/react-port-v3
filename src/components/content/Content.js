7c0ea45783f8d66d6d7cdfa895772b9ea2c8770e
Deepjyoti Roy <deepjyotiroy079@gmail.com>
2021-05-17 15:09:18 +0530

    Able to read markdown from database

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,6 +1,7 @@
 import { Facebook, Linkedin, GitHub, Instagram, Twitter } from 'react-feather';
 import Seperator from '../seperator/Seperator';
 import { Projects } from '../projects/Projects';
+import { Blogs } from '../blogs/Blogs';
 
 // eslint-disable-next-line
 const Content = () => {
@@ -67,6 +68,11 @@ const Content = () => {
             <div>
                 <p className="headingLg">Highlighted Projects</p>
                 <Projects />
+            </div>
+            <Seperator my={[4, 5]} width={['50%', '25%']} height={24} />
+            <div>
+                <p className="headingLg">Few things I've wrote recently</p>
+                <Blogs />
             </div>
             <Seperator my={[4, 5]} width={['50%', '25%']} height={24} />
         </>
